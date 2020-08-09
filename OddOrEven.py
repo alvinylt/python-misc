6e985d70b02c7c5f6c83d24baa593efac12ada5d
Alvin Tang <53111300+alvinylt@users.noreply.github.com>
2020-08-09 19:25:57 +0800

    OddOrEven.py update: syntax improvement

--- OddOrEven.py
+++ OddOrEven.py
@@ -1,4 +1,6 @@
-num = input('Type an integer here: ')
-if (-1)**int(num) == 1:
-    print('This is an even number.')
-else: print('This is an odd number.')
+num = int(input('Type an integer here: '))
+
+if (-1) ** num == 1:
+  print('This is an even number.')
+else:
+  print('This is an odd number.')
